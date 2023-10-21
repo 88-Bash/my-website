84ad67bf6a8f0b9e4061eefe0aa7d80198afc94f
Sebastian Szajuk <szajuk.sebastian@gmail.com>
2023-10-21 15:10:10 +0200

    structure for all sections of the webside initialized, and styles

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("home").addEventListener("click", function(){
+    fetch('./sections/home.txt')
+})
+
+document.getElementById("projects").addEventListener("click", function(){
+    fetch('./sections/projects.txt')
+})
+
+document.getElementById("contacts").addEventListener("click", function(){
+    fetch('./sections/contacts.txt')
+})
